73f184fcf00a71ffbf40ea76091405c5d197ace4
Praveen Selvaraj <praveen.selvaraj997@gmail.com>
2022-12-19 20:12:37 +0530

    Updated LinkedList Problem

---
+++ Problems/LinkedList/PartialReverse.cs
@@ -0,0 +1,53 @@
+﻿namespace Advanced_DSA.Problems.LinkedList;
+
+public static class PartialReverse
+{
+    public static ListNode ReverseList(ListNode A, int B)
+    {
+        ListNode temp = A, cut = null, curr = A;
+
+        while (curr != null)
+        {
+            int cnt = 1;
+            ListNode prev = null;
+            while (cnt <= B && curr != null)
+            {
+                ListNode nxt = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = nxt;
+                cnt++;
+            }
+            if (cut == null) cut = prev;
+            else
+            {
+                temp = cut;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = prev;
+            }
+        }
+        return cut;
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode? next;
+    public ListNode(int x) { this.val = x; this.next = null; }
+}
+
+/*
+        ListNode A = new ListNode(1);
+        var temp = A;
+        for(int i=2; i<=6; i++)
+        {
+            temp.next = new ListNode(i);
+            temp = temp.next;
+        }
+        var ans = PartialReverse.ReverseList(A, 3);
+        Console.WriteLine(ans.val);
+ */
--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
 ﻿using Advanced_DSA.Problems.Bit_Manipulation;
+using Advanced_DSA.Problems.LinkedList;
+
 public class Program
 {
     public static void Main(string[] args)
     {
-        var res = NPlus2.Missing2Numbers();
-        Console.WriteLine(res[0]);
-        Console.WriteLine(res[1]);
+        ListNode A = new ListNode(1);
+        var temp = A;
+        for(int i=2; i<=6; i++)
+        {
+            temp.next = new ListNode(i);
+            temp = temp.next;
+        }
+
+
+        var ans = PartialReverse.ReverseList(A, 3);
+        Console.WriteLine(ans.val);
     }
 }
